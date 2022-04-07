a43131c21bb296bb62e88b6e4998b9d4c5401cca
Carlos Hernandez <car2262541@maricopa.edu>
2022-04-07 14:36:31 -0700

    Implemented the Contact Page -added functional link opening to this repo link in the View -Filled out information regarding Contact

--- MVVM/View/ContactView.xaml.cs
+++ MVVM/View/ContactView.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace TabletMaster.MVVM.View
 {
@@ -24,5 +14,15 @@ namespace TabletMaster.MVVM.View
         {
             InitializeComponent();
         }
+
+        private void OnRepoButtonClicked(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(new ProcessStartInfo
+            {
+                FileName = "https://github.com/carlosalonsohernandez/tablet-master",
+                UseShellExecute = true
+            });
+        }
     }
+
 }
